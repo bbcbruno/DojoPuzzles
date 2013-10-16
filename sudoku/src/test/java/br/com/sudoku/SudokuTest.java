b4a4f932f82431981dd244894dcd0566a4725e8e
Priscila Carvalho <pri.carvalho86@gmail.com>
2013-10-16 20:41:07 -0300

    ajuste na nomenclatura de variaveis

--- sudoku/src/test/java/br/com/sudoku/SudokuTest.java
+++ sudoku/src/test/java/br/com/sudoku/SudokuTest.java
@@ -21,16 +21,16 @@ public class SudokuTest {
 	@Test
 	public void jogarNaGrade1x1oValor8NaPosicao1x1() {
 		Tabuleiro tabuleiro = new Sudoku().comGridsTamanho(3,3);
-		Grid grid1x1 = tabuleiro.jogarNaGridDe(new Posicao(1,1)).valor(8).para(new Posicao(1, 1));
-		assertEquals(8, grid1x1.getValorNa(new Posicao(1, 1)));
+		Grid grid = tabuleiro.jogarNaGridDe(new Posicao(1,1)).valor(8).para(new Posicao(1, 1));
+		assertEquals(8, grid.getValorNa(new Posicao(1, 1)));
 		assertEquals(8, tabuleiro.getGridNa(new Posicao(1, 1)).getValorNa(new Posicao(1, 1)));
 	}
 	
 	@Test
 	public void jogarNaGrade2x1oValor5NaPosicao1x1() {
 		Tabuleiro tabuleiro = new Sudoku().comGridsTamanho(3,3);
-		Grid grid1x1 = tabuleiro.jogarNaGridDe(new Posicao(2,1)).valor(5).para(new Posicao(1, 1));
-		assertEquals(5, grid1x1.getValorNa(new Posicao(2, 1)));
+		Grid grid = tabuleiro.jogarNaGridDe(new Posicao(2,1)).valor(5).para(new Posicao(1, 1));
+		assertEquals(5, grid.getValorNa(new Posicao(2, 1)));
 		assertEquals(5, tabuleiro.getGridNa(new Posicao(2, 1)).getValorNa(new Posicao(1, 1)));
 	}
 
