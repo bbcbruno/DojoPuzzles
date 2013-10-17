8f77cc8ac733ac153b1356a22eedf1e64dcc64a3
Priscila Carvalho <pri.carvalho86@gmail.com>
2013-10-17 12:45:43 -0300

    melhoria na descricao dos testes

--- sudoku/src/test/java/br/com/sudoku/SudokuTest.java
+++ sudoku/src/test/java/br/com/sudoku/SudokuTest.java
@@ -5,14 +5,15 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
+import br.com.sudoku.exception.ValorMaximoExcedido;
 import br.com.sudoku.jogo.Sudoku;
-import br.com.sudoku.jogo.Tabuleiro;
 import br.com.sudoku.model.Posicao;
+import br.com.sudoku.model.Tabuleiro;
 
 public class SudokuTest {
 
 	@Test
-	public void criarUmNovoJogoComGridsDeTamanho3x3() {
+	public void umTabuleiroDeTamanho3x3DeveConter9Grids() {
 		
 		Tabuleiro tabuleiro = new Sudoku().comGridsTamanho(3,3);
 		
@@ -21,7 +22,7 @@ public class SudokuTest {
 	}
 
 	@Test
-	public void jogarNaGrade1x1oValor8NaPosicao1x1() {
+	public void umGridNaGrade1x1NaPosicao1x1DeveGuardarOValorPassado8() {
 		
 		Tabuleiro tabuleiro = new Sudoku().comGridsTamanho(3,3);
 		tabuleiro.jogarNaGridDe(new Posicao(1,1)).valor(8).para(new Posicao(1, 1));
@@ -30,12 +31,20 @@ public class SudokuTest {
 	}
 	
 	@Test
-	public void jogarNaGrade2x1oValor5NaPosicao1x1() {
+	public void umGridNaGrade2x1NaPosicao1x1DeveGuardarOValorPassado5() {
 		
 		Tabuleiro tabuleiro = new Sudoku().comGridsTamanho(3,3);
 		tabuleiro.jogarNaGridDe(new Posicao(2,1)).valor(5).para(new Posicao(1, 1));
 		
 		assertEquals(new Integer(5), tabuleiro.getGridNa(new Posicao(2, 1)).getValorNa(new Posicao(1, 1)));
+	}
+
+	@Test(expected=ValorMaximoExcedido.class)
+	public void quandoJogarUmNumeroMaiorQueOMaximoPermitidoNoJogoDeveGerarExcecao() {
+		
+		Tabuleiro tabuleiro = new Sudoku().comGridsTamanho(3,3);
+		tabuleiro.jogarNaGridDe(new Posicao(2,1)).valor(18).para(new Posicao(1, 1));
+		
 	}
 
 }
